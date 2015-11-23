e9d06e68533bf589b2b62b629308e234bfba393f
James Beal <James.Beal@sanger.ac.uk>
2015-11-23 12:09:34 +0000

    Add license

--- scripts/unpack_data.sh
+++ scripts/unpack_data.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
+#License
+#=======
+#  Copyright (c) 2015 Genome Research Ltd. 
+#
+#  Author: James Beal <James.Beal@sanger.ac.uk>
+#
+#  This file is part of https://github.com/wtsi-ssg/os-cluster-internal-config
+#
+#  This  is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.   This program is distributed
+#  in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+#  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+#  PARTICULAR PURPOSE. See the GNU General Public License for more details. 
+#  You should have received a copy of the GNU General Public License along
+#  with this program. If not, see <http://www.gnu.org/licenses/>. 
+#
 mkdir -p /opt
 cd /opt
 tar xf ~ubuntu/openlava.tar.gz      
